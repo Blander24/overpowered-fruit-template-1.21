1260d8185be04bbadd66da3243963d4dc4ff7322
Blander24 <benlands101@gmail.com>
2024-09-11 22:37:43 -0400

    Initial commit

---
+++ src/main/java/com/benji/overpoweredfruit/OverpoweredFruitDataGenerator.java
@@ -0,0 +1,11 @@
+package com.benji.overpoweredfruit;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class OverpoweredFruitDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
